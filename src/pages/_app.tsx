ca2ca63b549e7644407af86da7bc5137cbb565c3
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-04-05 14:29:11 -0400

    refactor: remove authentication to make only with template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,14 @@
 import { AppProps } from 'next/app';
 import { Layout } from '@/layout';
-import { SessionProvider } from 'next-auth/react';
 import '@/styles/fonts.css';
 import '@/styles/resets/reset.css';
 import '@/styles/resets/_modern-normalize.css';
 
-export const MyApp = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) => {
+export const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 };
 
--- src/pages/login.tsx
+++
@@ -1,8 +0,0 @@
-import { signIn } from 'next-auth/react';
-import React from 'react';
-
-export const Login = () => {
-  return <button onClick={() => signIn(`github`)}>GitHub</button>;
-};
-
-export default Login;
